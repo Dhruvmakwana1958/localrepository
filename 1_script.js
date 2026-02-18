c66a991fc6db9d30d46f57f92cf818465d2f4530
Dhruvmakwana1958 <makwanadhruv1958@gmail.com>
2026-02-18 23:02:55 +0530

    initial file

---
+++ 1_script.js
@@ -0,0 +1,12 @@
+console.log(window.document); // ana thi html no code print thaasse 
+console.dir(document)         // ana thi document ni property open thse ane biju ae pan noticable che ke ahi apde window.document na karyu 
+// enu karan che ke bowser no je aa document wadu objet chce ane jeni andar bija sub object che teni by default aa code ne khbr j hoy che tene
+// alag thi kevu nthi padtu to ene define karvani jarur nthi baki apde jo print karvu hoy to aa rite b kari saki windows.console.log("hiii")
+
+// console.log => string print karava
+// console.dir => methods , property print karava
+
+console.dir(document.body.childNodes[1]);
+window.document.body.style.background = "green";
+document.body.childNodes[3].innerText = "ABCD"
+
